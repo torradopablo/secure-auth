eb5c5c25d8e909edacb4166fbbd094f2ed1c7610
Pablo Torrado <torradopablo1@gmail.com>
2023-05-17 19:36:20 -0300

    Add config cookies and redirect home ui

---
+++ server/src/api.ts
@@ -0,0 +1,83 @@
+import express from 'express';
+import passport from 'passport';
+import * as PassportGithub  from 'passport-github2'
+import bp from 'body-parser';
+import cors from 'cors';
+import cookieParser from 'cookie-parser'
+import expressSession from 'express-session'
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
+dotenv.config();
+
+const jwtSecret = process.env.JWT_SECRET
+
+passport.use(new PassportGithub.Strategy({
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: "http://localhost:3100/auth/github/callback"
+},
+async function(accessToken:any, refreshToken, profile, done) {    
+    try{
+      return done(null,profile)
+    } catch (error) {
+      return done(error,false)
+    }
+}
+));
+
+passport.serializeUser((user,done)=> {
+  console.log('Serialize User')
+  try {
+    done(null,user);
+  } catch (error) {
+    done(error,null)
+  } 
+});
+
+passport.deserializeUser((user,done)=>{
+  console.log('Deserialize User')
+  try {
+     done(null,user)
+  }  catch(error) {
+     done(error,null)
+  }
+})
+
+const app = express()
+const port = process.env.API_PORT
+
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(expressSession({ secret: jwtSecret, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+app.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/failure' }),
+  function(req, res) {
+    
+    const token = jwt.sign({user:req.user},jwtSecret,{
+      expiresIn: "1y",
+    });
+    
+    res.cookie("access_token", token)
+    res.redirect(`${process.env.HOME_UI}`);
+  });
+
+app.get('/failure', (req,res)=> res.send('Your auth is not good'))  
+
+
+app.listen(port || 3100, () => {
+  console.log(`⚡️[API]: Server is running at http://localhost:${port}`)
+});
+
+
+export default app;
--- server/src/index.ts
+++ server/src/index.ts
@@ -5,13 +5,7 @@ import resolvers from './graphql/resolvers';
 import dotenv from 'dotenv';
 import jwt from 'jsonwebtoken';
 import { GraphQLError } from 'graphql';
-import express from 'express';
-import passport from 'passport';
-import * as PassportGithub  from 'passport-github2'
-import bp from 'body-parser';
-import cors from 'cors';
-import cookieParser from 'cookie-parser'
-import expressSession from 'express-session'
+import app from './api';
 
 dotenv.config();
 
@@ -53,92 +47,4 @@ const { url } = await startStandaloneServer(server, {
 
 console.log(`🚀 Apollo server ready at: ${url}`);
 
-
-var userData:any;
-
-passport.use(new PassportGithub.Strategy({
-  clientID: process.env.GITHUB_CLIENT_ID,
-  clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  callbackURL: "http://localhost:3100/auth/github/callback"
-},
-async function(accessToken:any, refreshToken, profile, done) {
-    
-    try{
-      console.log(profile)
-      return done(null,profile.id)
-    } catch (error) {
-      return done(error,false)
-    }
- 
-}
-));
-
-
-passport.serializeUser((user:any,done)=> {
-  console.log('Serializing User')
-  console.log(user);
-  userData=user;
-   done(null,user.id);
-});
-
-passport.deserializeUser((id,done)=>{
-  console.log('Deserialize User User')
-  console.log(id)
-  try {
-     done(null,userData.id)
-  }  catch(error) {
-     done(error,null)
-  }
-})
-
-const app = express()
-const port = process.env.API_PORT
-
-/*
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/dashboard.html');
-});
-
-
-app.get('/login', (req, res) => {
-  res.sendFile(__dirname + '/login.html');
-});
-
-*/
-
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-app.use(cors());
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(expressSession({ secret: jwtSecret, resave: true, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session()); 
-
-//auth
-app.get('/auth/github',
-  passport.authenticate('github', { scope: [ 'user:email' ] }));
-
-app.get('/auth/github/callback', 
-  passport.authenticate('github', { failureRedirect: '/failure' }),
-  function(req, res) {
-    console.log(req)
-    /*
-    const token = jwt.sign({user:req.user},jwtSecret,{
-      expiresIn: "1y",
-    });
-    console.log(token)*/
-
-    //res.cookie("access_token", token)
-    res.redirect('/');
-  });
-
-app.get('/failure', (req,res)=> res.send('Your auth is not good'))  
-
-
-app.listen(port || 3100, () => {
-  console.log(`⚡️[API]: Server is running at http://localhost:${port}`)
-});
-
-
+app;
